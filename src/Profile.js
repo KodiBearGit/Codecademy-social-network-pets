ea80d9ec830cde00f4daf968e96a02b47f1214e1
Chris <chris.tisserat@gmail.com>
2021-01-13 14:26:00 -0800

    4of10 corrected typo holding back function of name loading

--- src/Profile.js
+++ src/Profile.js
@@ -3,24 +3,26 @@ import { fetchUserData, cancelFetch } from './dataFetcher';
 import { Userlist } from './Userlist';
 
 export class Profile extends React.Component {
+  //1of10 added constructor
   constructor(props) {
     super(props);
     this.state = {
       userData: null
     }
   }
-
+  //2of10 looks correct to me
   loadUserData() {
     this.setState({userData: null})
     this.fetchID = fetchUserData(this.props.username, (userData) => {
-      this.setState({userData});
+      this.setState({ userData });
     });
   }
 
   render() {
-    const isLoading = (this.state.userDate === null) ? true : false; 
-      
-
+    //4of10 looks to be working now i had mispelled this
+    let isLoading = (this.state.userData === null) ? true : false;
+    //5of10 items this doesn't work here 
+    const name = isLoading ? 'Loading animal info' : this.state.userData.name;
     let className = 'Profile';
     if (isLoading) {
       className += ' loading';
@@ -30,7 +32,7 @@ export class Profile extends React.Component {
       <div className={className}>
         <div className="profile-picture"></div>
         <div className="profile-body">
-          <h2>Name goes here</h2>
+          <h2>{name}</h2>
           <h3>@{this.props.username}</h3>
           <p>Bio goes here</p>
           <h3>My friends</h3>
